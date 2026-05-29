2e8c34e9e83bf44256acd6a665623b01eb5c76f9
Himanshu Bisht <himanshubishtcse@gmail.com>
2026-05-29 17:27:30 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, inorder, 0, preorder.length-1, 0, preorder.length-1);
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd)
+    {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+        
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int posInInorder = findValInInorder(inorder, root.val);
+        int leftTreeSize = posInInorder - inStart;
+        root.left = buildTree(preorder, inorder, preStart+1, preStart+leftTreeSize, inStart, posInInorder-1);
+        root.right = buildTree(preorder, inorder, preStart+leftTreeSize+1, preEnd, posInInorder+1, inEnd);
+        return root;
+    }
+
+    public int findValInInorder(int[] inorder, int val)
+    {
+        for(int i=0;i<inorder.length;i++)
+            if(inorder[i] == val)
+                return i;
+        return -1;
+    }
+}
