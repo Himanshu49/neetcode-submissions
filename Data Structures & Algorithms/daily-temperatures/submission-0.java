856342fe09f0ba3bcbd751ebe028a33f9421e8e1
Himanshu Bisht <himanshubishtcse@gmail.com>
2026-07-11 18:07:23 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    class Pair{
+        int temp;
+        int day;
+        public Pair(int t, int d){
+            temp = t;
+            day = d;
+        }
+    }
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Pair> record = new Stack<>();
+        int n = temperatures.length;
+        int res[] = new int[n];
+        for(int i=0;i<n;i++){
+            while(record.size() > 0 && record.peek().temp < temperatures[i]){
+                Pair curr = record.pop();
+                res[curr.day] = i - curr.day;
+            }
+            record.push(new Pair(temperatures[i], i));
+        }
+
+        while(record.size() > 0){
+            Pair curr = record.pop();
+            res[curr.day] = 0;
+        }
+        return res;
+    }
+}
